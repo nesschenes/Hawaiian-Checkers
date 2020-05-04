653df35f317b33bc659e75301102c4bec937e19a
NessChen <jaes54321@gmail.com>
2020-05-04 23:15:55 +0800

    Add camera scaler to achieve RWD

---
+++ Assets/Scripts/Utilities/ScreenUtility.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Hawaiian.Utility
+{
+    public static class ScreenUtility
+    {
+        public static float Width => UnityEngine.Camera.main.ViewportToWorldPoint(new Vector2(1, 1)).x * 2;
+
+        public static float Height => UnityEngine.Camera.main.ViewportToWorldPoint(new Vector2(1, 1)).y * 2;
+
+        /// <summary> Width / Height </summary>
+        public static float Ratio => (float)Screen.width / Screen.height;
+    }
+}
