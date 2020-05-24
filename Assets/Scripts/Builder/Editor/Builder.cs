f44198cae95e23c85e2d91496fa27b68f4713dbe
NessChen <jaes54321@gmail.com>
2020-05-24 15:26:20 +0800

    Add BuildViaCommandLine.

--- Assets/Scripts/Builder/Editor/Builder.cs
+++ Assets/Scripts/Builder/Editor/Builder.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
@@ -62,6 +65,76 @@ namespace Konane.Builder
             {
                 Debug.Log("[Android] Build failed");
             }
+        }
+
+        static void BuildViaCommandLine()
+        {
+            ParseCommandLine();
+
+            PlayerSettings.bundleVersion = Version;
+            var destinationPath = Path.Combine(DestinationPath, PlayerSettings.productName);
+            destinationPath += GetExtension();
+
+            BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, destinationPath, EditorUserBuildSettings.activeBuildTarget, BuildOptions.None);
+        }
+
+        static string Version;
+        static string DestinationPath;
+        static void ParseCommandLine()
+        {
+            Dictionary<string, Action<string>> cmdActions = new Dictionary<string, Action<string>>
+            {
+                {
+                    "--buildingVersion", delegate(string argument)
+                    {
+                        Version = argument;
+                    }
+                },
+                {
+                    "--buildingFolder", delegate(string argument)
+                    {
+                        DestinationPath = argument;
+                    }
+                }
+            };
+
+            Action<string> actionCache;
+            var cmdArguments = Environment.GetCommandLineArgs();
+            for (var count = 0; count < cmdArguments.Length; count++)
+            {
+                if (cmdActions.ContainsKey(cmdArguments[count]))
+                {
+                    actionCache = cmdActions[cmdArguments[count]];
+                    actionCache(cmdArguments[count + 1]);
+                }
+            }
+
+            if (string.IsNullOrEmpty(Version))
+            {
+                Version = Application.version;
+            }
+
+            if (string.IsNullOrEmpty(DestinationPath))
+            {
+                DestinationPath = Path.GetDirectoryName(Application.dataPath);
+            }
+        }
+
+        static string GetExtension()
+        {
+            var extension = string.Empty;
+            switch (EditorUserBuildSettings.activeBuildTarget)
+            {
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    extension = ".exe";
+                    break;
+                case BuildTarget.Android:
+                    extension = ".apk";
+                    break;
+            }
+
+            return extension;
         }
     }
 }
